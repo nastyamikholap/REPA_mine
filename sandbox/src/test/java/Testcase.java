b181abdcef4e52436f51d5c05c9e15fb32a43a03
nastyamikholap <nastyamikholap@users.noreply.github.com>
2020-09-30 14:33:34 +0300

    Добавление TestNG

---
+++ sandbox/src/test/java/Testcase.java
@@ -0,0 +1,14 @@
+/*
+1. setup
+2. login
+3.close
+
+ */
+import org.testng.annotations.Test;
+
+public class Testcase {
+
+    @Test
+    void setup()
+    {System.out.println("Test case test");}
+}
