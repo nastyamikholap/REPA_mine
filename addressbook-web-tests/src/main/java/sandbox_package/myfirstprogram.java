f4e2144bf483418682ebc060b3a534dbaf9d72df
nastyamikholap <nastyamikholap@users.noreply.github.com>
2020-09-30 17:55:27 +0300

    Добавлены классы селениум

---
+++ addressbook-web-tests/src/main/java/sandbox_package/myfirstprogram.java
@@ -0,0 +1,9 @@
+package sandbox_package;
+
+public class myfirstprogram {
+
+public static void main(String[] args)
+{
+
+System.out.println("Hello, world");
+}}
---
+++ addressbook-web-tests/src/test/java/testpackage/Testcase.java
@@ -0,0 +1,11 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class Testcase {
+
+@Test
+  public void testArea()
+            {System.out.println("Test case test");
+            }
+}
