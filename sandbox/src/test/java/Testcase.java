742f6f1f3857f567cbd46a443989164210d7ad08
nastyamikholap <nastyamikholap@users.noreply.github.com>
2020-09-30 14:50:46 +0300

    добавление теста

--- sandbox/src/test/java/Testcase.java
+++ sandbox/src/test/java/Testcase.java
@@ -9,6 +9,10 @@ import org.testng.annotations.Test;
 public class Testcase {
 
     @Test
-    void setup()
+    void setupcase()
     {System.out.println("Test case test");}
+
+    @Test
+    void setupsecond()
+    {System.out.println("Test case test second");}
 }
